adde281171b3277920ef4e07a9fa07436dd37bae
Gideon Mutai <giddymutai13@gmail.com>
2024-03-19 06:51:14 +0300

    Concatenates the string literals

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`
+// backticks are used for interpolation
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)} !`
